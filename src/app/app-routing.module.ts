c5d71044c63048a6edbb2711152cc34bcfdc9e87
strutt3r <shawnwwerner@gmail.com>
2022-04-07 16:51:05 -0500

    1 file modified, 4 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,13 @@ import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
 import { HomeComponent } from './home/home.component';
 import { ViewEmployeeComponent } from './view-employee/view-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'Home', pathMatch: 'full' },
+  { path: 'Home', component: HomeComponent },
+  { path: 'ViewEmployee/:employeeid', component: ViewEmployeeComponent },
+  { path: 'AddEmployee', component: AddEmployeeComponent },
+  { path: 'EditEmployee/:employeeid', component: EditEmployeeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
